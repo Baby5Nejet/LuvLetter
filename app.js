b3c55e9c6275f74299402ade2c38bb4331bfc89a
Baby777 <409178491@qq.com>
2021-07-15 13:50:38 +0800

    promise

--- app.js
+++ app.js
@@ -13,13 +13,18 @@ app.use(async (ctx,next)=>{
   // ctx   上下文
   // next 下一个中间件函数
   // 要加async和await来确保异步调用顺序的正确性
-  console.log('1',next)
+  console.log('1','next')
+  const a = next()
+  console.log(a)
   console.log('halo word')
-  await next()
+  // await next()   // next调用结果是promise
 })
 
 app.use(()=>{
+  console.log('2','next')
   console.log('halo nejet')
+  // return 'abc'
+  // return new Promise() 也可以return promise
 })
 
 app.listen(8080)
